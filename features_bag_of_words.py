929a2c26668a4dff48f2b7b1dbf4856e655e4689
Deepti Kochar <deepti.kochar12@gmail.com>
2015-11-21 17:18:11 -0500

    Removed extra bracket

--- features_bag_of_words.py
+++ features_bag_of_words.py
@@ -20,7 +20,7 @@ def process_data(s3key):
     """
     print "FETCH_DATA gets called"
     conn = boto.connect_s3()
-    b = conn.get_bucket(bucket, validate=False))
+    b = conn.get_bucket(bucket, validate=False)
     k = b.get_key(s3key)
     data = k.get_contents_as_string()
     conn.close()
