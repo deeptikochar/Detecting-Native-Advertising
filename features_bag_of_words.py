f3191b24dcda75a43ad88aa33805d7fb8c7cdb1c
Seema Suresh <seemas2309@gmail.com>
2015-11-21 19:04:18 -0500

    Changed so beautifulsoup works!

--- features_bag_of_words.py
+++ features_bag_of_words.py
@@ -8,35 +8,12 @@ from bs4 import BeautifulSoup
 
 
 bucket = 'bds-new'
-def process_data(s3key):
-    """
-    Fetch data with the given s3 key and pass along the contents as a string.
-
-    :param s3key: An s3 key path string.
-    :return: A tuple (file_name, data) where data is the contents of the 
-        file in a string. Note that if the file is compressed the string will 
-        contain the compressed data which will have to be unzipped using the 
-        gzip package.
-    """
+def process_data(s3keyDict):
     print "FETCH_DATA gets called"
-    conn = boto.connect_s3()
-    b = conn.get_bucket(bucket, validate=False)
-    k = b.get_key(s3key)
-    data = k.get_contents_as_string()
-    conn.close()
-    soup = BeautifulSoup(data)
-    soup.get_text().replace("\n",'')
-    for script in soup(["script", "style"]):
-        script.extract()    # rip it out
-    text = soup.get_text()
-    # break into lines and remove leading and trailing space on each
-    lines = (line.strip() for line in text.splitlines())
-    # break multi-headlines into a line each
-    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    text = '\n'.join(chunk for chunk in chunks if chunk)
-    words = re.findall('\w+', text)
+    
+    words = re.findall('\w+', s3keyDict["content"])
     bag_of_words = {}
-    bag_of_words['filename'] = os.path.basename(s3key)
+    bag_of_words['filename'] = os.path.basename(s3keyDict["filename"])
     num_distinct_words = 0
     for word in words:
         if word in bag_of_words:
@@ -46,25 +23,37 @@ def process_data(s3key):
             num_distinct_words +=1
     bag_of_words['total_words'] = len(words)
     bag_of_words['distinct_words'] = num_distinct_words    
-    return os.path.basename(s3key), bag_of_words
+    return os.path.basename(s3keyDict["filename"]), bag_of_words
 
 conn = boto.connect_s3()
-# bucket is the name of the S3 bucket where your data resides
 
-b = conn.get_bucket(bucket, validate=False) 
-# inkey_root is the S3 'directory' in which your files are located
-inkey_root = '3'
+b = conn.get_bucket(bucket, validate=False)  
+inkey_root = 'temp'
 keys = b.list(prefix=inkey_root)
 
 key_list = [key.name for key in keys]
 
 key_list.pop(0)
 
+data = []
+
+for s3key in key_list:
+    k = b.get_key(s3key)
+    dictTemp = {}
+    dictTemp["filename"] = s3key
+    soup = BeautifulSoup(k.get_contents_as_string())
+    soup.get_text().replace("\n",'')
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+    text = soup.get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    dictTemp["content"] = text
+    data.append(dictTemp)
 conn.close()
 
-#sc = pyspark.SparkContext('local', 'Whatever')
-# Create an RDD from the list of s3 key names to process stored in key_list
-file_list = sc.parallelize(key_list)
+file_list = sc.parallelize(data)
 
 p = file_list.flatMap(process_data).collect()
 
@@ -73,7 +62,7 @@ for i in range(1, len(p), 2):
     fieldnames = fieldnames.union(set(p[i].keys()))
 
 fieldnames = list(fieldnames)
-with open('3-formatted.csv', 'w') as csvfile:
+with open('temp-formatted.csv', 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     headers = {}
     for field in fieldnames:
@@ -86,5 +75,3 @@ with open('3-formatted.csv', 'w') as csvfile:
                 if field not in dom:
                     dom[field] = 0
             writer.writerow(dom)
-
-
